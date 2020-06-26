abb33468a714d9b787ba43c1971f7b3517cb0155
ruaknim <Ivan.korniasov@raiffeisen.ru>
2020-06-26 11:00:31 +0300

    v3

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Здесь будет Timer Риты v2
+          Здесь будет Timer Риты v3
         </p>
         <a
             className="App-link"
